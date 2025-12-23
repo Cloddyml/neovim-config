58eb52d3befb898da96e28409ae98fbd3256c94e
Couguar <ingluemlp@gmail.com>
2025-12-24 00:11:29 +0300

    add: added mason plugin to use LSP with pre-installed lua-ls, pyright, rust_analyzer

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,27 @@
+-- LSP Configuration
+
+return {
+  enabled = true,
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    }
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = { 
+      ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+}
+}
